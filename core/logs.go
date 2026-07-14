b8ec3c0334e4960576fc637d3d205eec34036afd
CarlosSenra <crsbsenra@gmail.com>
2026-07-14 15:26:55 -0300

    feat: read & write files log

--- core/logs.go
+++ core/logs.go
@@ -1,7 +1,36 @@
 package core
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func ExibirLogs() {
-	fmt.Println("Mostrando logs...")
+func EscreverEmArquivo(Path string, Mensagem string) {
+	arquivo, err := os.OpenFile(Path, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println("Erro ao abrir o arquivo de logs:", err)
+		return
+	}
+	defer arquivo.Close()
+
+	_, err = arquivo.WriteString(Mensagem + "\n")
+	if err != nil {
+		fmt.Println("Erro ao escrever no arquivo de logs:", err)
+	}
+}
+
+func ExibirLogs(path string) {
+	arquivo, err := os.OpenFile(path, os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println("Erro ao abrir o arquivo de logs:", err)
+		return
+	}
+	defer arquivo.Close()
+
+	dados, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("Erro ao ler o arquivo de logs:", err)
+		return
+	}
+	fmt.Println(string(dados))
 }
